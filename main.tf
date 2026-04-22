dd916f5e95c1a6d95293d3e445ecb79686c94e71
Ryan McAlary <ryan.mcalary@justice.gov.uk>
2026-04-22 09:37:20 +0100

    DTSPO-30203 - Grant new Jenkins MIs access to keyvault

--- main.tf
+++ main.tf
@@ -10,12 +10,18 @@ resource "azurerm_resource_group" "rg" {
   tags     = var.common_tags
 }
 
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 module "key_vault" {
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                = local.key_vault_name
   product             = var.product
   env                 = var.env
   object_id           = var.jenkins_AAD_objectId
+  jenkins_object_id   = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name = azurerm_resource_group.rg.name
   product_group_name  = "DTS FaCT"
   common_tags         = var.common_tags
